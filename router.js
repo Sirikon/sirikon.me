61feb87fe7ab5ec48b4bfd545583e57aefe6135b
Carlos Fernández Llamas <hello@sirikon.me>
2018-12-02 13:46:11 +0100

    Add favicons.

--- router.js
+++ router.js
@@ -1,9 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+function getFaviconsFiles() {
+    const faviconsFolderPath = path.join(__dirname, '/src/_src/favicons');
+    return fs.readdirSync(faviconsFolderPath);
+}
+
 module.exports = {
     auto: {
         exclude: '_src'
     },
     custom: (data) => {
         const result = {};
+
+        getFaviconsFiles().forEach((file) => {
+            result[file] = { target: `_src/favicons/${file}` };
+        });
 
         result['index.xml'] = { target: '_src/rss.ejs' }
         

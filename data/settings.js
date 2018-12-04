421bc5565fa29945fc506e49206cf4f3adb78814
Carlos Fernández Llamas <hello@sirikon.me>
2018-12-04 18:56:51 +0100

    Add Matrix profile link. Remove XMPP. Parametrize. Open Graph metadata.

--- data/settings.js
+++ data/settings.js
@@ -1,9 +1,36 @@
+const me = {
+    name: 'Carlos',
+    surname: ['Fernández', 'Llamas'],
+    get fullName() { return `${me.name} ${me.surname.join(' ')}` },
+    alias: 'Sirikon'
+};
+
 module.exports = {
     data() {
         return {
-            title: 'sirikon.me',
-            description: 'Carlos Fernández Llamas personal website.',
-            url: process.env.SIRIKONME_URL || 'http://localhost:3000'
+            title: 'Sirikon.me',
+            description: `${me.fullName} personal website.`,
+            url: process.env.SIRIKONME_URL || 'http://localhost:3000',
+            footer: `${me.alias} - 2018`,
+
+            me,
+
+            about: {
+                facts: [
+                    ["Born in", "Málaga, Andalusia", "https://www.openstreetmap.org/relation/340746"],
+                    ["Living in", "Bilbao, Basque Country", "https://www.openstreetmap.org/relation/339549"],
+                    ["Working on", "Plain Concepts", "https://www.plainconcepts.com/"],
+                    ["Mostly", "programming", "https://github.com/sirikon/"],
+                    ["Even some", "games", "https://sirikon.itch.io/"],
+                    ["Doing some", "photography", "https://500px.com/sirikon"],
+                    ["Talking on", "the fediverse", "https://plaza.remolino.town/users/402"],
+                ],
+                contact: [
+                    ["Email", "hello@sirikon.me", "mailto:hello@sirikon.me"],
+                    ["ActivityPub", "@sirikon@plaza.remolino.town", "https://plaza.remolino.town/users/402"],
+                    ["Matrix", "@sirikon:portico.remolino.town", "https://matrix.to/#/@sirikon:portico.remolino.town"],
+                ]
+            }
         }
     }
 }

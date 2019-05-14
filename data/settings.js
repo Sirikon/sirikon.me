0e706aa0d43b42d1a16efd547b9358a4cc64f2a4
Sirikon <hello@sirikon.me>
2019-05-14 21:53:53 +0200

    Add CV

--- data/settings.js
+++ data/settings.js
@@ -5,13 +5,62 @@ const me = {
     alias: 'Sirikon'
 };
 
+const job = {
+    get currentPosition() { return job.positions[0] },
+    positions: [
+        {
+            role: 'Software Developer',
+            company: {
+                name: 'Plain Concepts',
+                url: 'https://www.plainconcepts.com/',
+            },
+            since: 'September 2015',
+            until: 'Today',
+            text: [
+                'Development, consulting and training role in the Bilbao office of Plain Concepts for multiple clients in many industries, both brand new and legacy projects.',
+                'In charge of development of many solutions, using the latest technologies available that suit with the project\'s requirements.',
+                'Working mostly with .NET Technologies, Azure Cloud, Web Technologies, Containers and Container Orchestration.',
+                'Also some experimentation with stuff like Microsoft Band, UWP and Xamarin.'
+            ]
+        },
+        {
+            role: 'I+D+i Project Manager',
+            company: {
+                name: 'Seis Cocos',
+                url: 'http://seiscocos.com/'
+            },
+            since: 'May 2013',
+            until: 'June 2015',
+            text: [
+                'Responsible of every project that requires cutting-edge web technologies, responsible of internal tools and frameworks development, main software architect and systems/services maintainer.',
+                'I accomplished great look&feel, functionality and stability in much lower time than other environments by using web technologies, this way the application was automatically multiplatform, saving time and money.',
+                'Most of the time, PHP, Node, Cordova, MySQL and MongoDB were involved, while dealing with On-Premise infrastructure.',
+                'Helped in game development with Unity Engine and developed an unreleased tamagotchi-like game for Samsung Galaxy Gear (Tizen).'
+            ]
+        },
+        {
+            role: 'Intern',
+            company: {
+                name: 'Ichton Software',
+                url: 'https://es.linkedin.com/company/ichton-software-s.l.'
+            },
+            since: 'February 2013',
+            until: 'June 2013',
+            text: [
+                'Made some web pages for clients and helped in other areas.',
+                'Mostly computer repair, WordPress installation, configuration and theme customization, and some static pages with plain HTML and CSS.'
+            ]
+        }
+    ]
+};
+
 module.exports = {
     data() {
         return {
             title: 'Sirikon.me',
             description: `${me.fullName} personal website.`,
             url: process.env.SIRIKONME_URL || 'http://localhost:3000',
-            footer: `${me.alias} - 2018`,
+            footer: `${me.alias}.me - 2019`,
 
             me,
 
@@ -29,7 +78,9 @@ module.exports = {
                     ["Email", "hello@sirikon.me", "mailto:hello@sirikon.me"],
                     ["ActivityPub", "@sirikon@plaza.remolino.town", "https://plaza.remolino.town/sirikon"],
                 ]
-            }
+            },
+
+            job,
         }
     }
 }

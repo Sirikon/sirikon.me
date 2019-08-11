96f7f01d16478b885b4ab70c7f50b3f7a643242a
Sirikon <hello@sirikon.me>
2019-08-11 13:54:49 +0200

    Update projects

--- data/projects.js
+++ data/projects.js
@@ -4,37 +4,32 @@ module.exports = {
             {
                 name: "Waves Out!",
                 description: "A frenetic party or single player game where magnetism is the protagonist. Should be available on April 2019 for PS4.",
-                link: "https://wavesoutgame.com/",
-                image: "wavesout.png"
+                link: "https://wavesoutgame.com/"
             },
             {
                 name: "Pitch",
                 description: "The Progressive & Scriptable Static Site Generator",
-                link: "https://github.com/sirikon/pitch",
-                image: "pitch.png"
+                link: "https://github.com/sirikon/pitch"
             },
             {
                 name: "Oilo Oilo",
                 description: "Oilo Oilo is a videogame where your objective is to defend the chickens... feeding them!",
-                link: "https://sirikon.itch.io/oilo-oilo",
-                image: "oilooilo.png"
+                link: "https://sirikon.itch.io/oilo-oilo"
             },
             {
                 name: "Luxury Madness",
                 description: "2D local multiplayer action videogame made in 48 hours for MalagaJam Weekend II with three more people.",
-                link: "https://sirikon.itch.io/luxury-madness",
-                image: "luxurymadness.png"
+                link: "https://sirikon.itch.io/luxury-madness"
             },
             {
                 name: "Unfollowers",
                 description: "Check who unfollowed you in a fast and simple way. No Ads, no unexpected twits, no problem!",
-                link: "https://unfollowers.sirikon.me/"
+                link: "https://github.com/sirikon/Unfollowers"
             },
             {
                 name: "Sirilog",
                 description: "Bukkit plugin with a simple and easy event logging system for admins who need to record everything that happens in their Minecraft servers, with MySQL support!",
-                link: "https://github.com/Sirikon/sirilog",
-                image: "sirilog.png"
+                link: "https://github.com/Sirikon/sirilog"
             }
         ]
     }
